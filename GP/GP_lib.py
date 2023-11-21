2a3a8ebfceb73e28194d89480267229c93a9aa01
Dawid Król <dawidkrol7@gmail.com>
2023-11-22 00:33:48 +0100

    Implementing basics of language in gp

--- GP/GP_lib.py
+++ GP/GP_lib.py
@@ -3,30 +3,194 @@ import copy
 import pickle
 import numpy as np
 
+used_variables = set()
+
 class Node:
     def __init__(self, value, children=None):
         self.value = value
         self.children = children or []
 
-def generate_random_program(max_depth, terminal_nodes, function_nodes):
-    if max_depth == 0 or (random.random() < terminal_nodes / (terminal_nodes + function_nodes)):
-        return Node(random.uniform(-1.0, 1.0))  # Terminal node (constant)
 
-    """Add values from language"""
-    value = random.choice(['+', '-', '*', '/'])  # Function node
-    children = [generate_random_program(max_depth - 1, terminal_nodes, function_nodes) for _ in range(2)]
-    return Node(value, children)
+def use_created_variable():
+    return Node(random.choice(list(used_variables)))
+
+def generate_new_variable():
+    new_variable = 'i' + str(random.randint(1, 99999))
+    while new_variable in used_variables:
+        new_variable = 'i' + str(random.randint(1, 99999))
+    used_variables.add(new_variable)
+    return Node(new_variable)
+
+
+def generate_operator():
+    return Node(random.choice(['+', '-', '*', '/']))
+
+
+def generate_number():
+    options = ['int', 'float']
+    option = random.choice(options)
+
+    if option == 'int':
+        return Node(str(random.randint(0, 9)))
+    elif option == 'float':
+        return Node(str(random.uniform(-1.0, 1.0)))
+
+def generate_number_or_used_values():
+    options = ['int', 'float']
+    if len(used_variables) > 0:
+        options.append('used_variables')
+
+    option = random.choice(options)
+    if option == 'int':
+        return Node(str(random.randint(0, 9)))
+    elif option == 'float':
+        return Node(str(random.uniform(-1.0, 1.0)))
+    elif option == 'used_variables':
+        return Node(str(random.choice(list(used_variables))))
+
+
+def generate_comparison_operator():
+    return Node(random.choice(['==', '!=', '<', '>', '<=', '>=']))
+
+
+def generate_logical_operator():
+    return Node(random.choice(['&&', '||']))
+
+
+def generate_assignment():
+    return Node('=')
+
+
+def generate_output_statement():
+    return Node('print')
+
+
+def generate_loop():
+    condition = generate_condition(max_depth, terminal_nodes, function_nodes)
+    return Node('for a in ', [condition, generate_block(max_depth, terminal_nodes, function_nodes)])
+
+
+def generate_block(max_depth, terminal_nodes, function_nodes):
+    return generate_program(max_depth - 1, terminal_nodes, function_nodes)
+
+
+def generate_logical_value():
+    return Node(random.choice(['true', 'false']))
+
+
+def generate_condition(max_depth, terminal_nodes, function_nodes):
+    options = ['number', 'constant', 'comparison_operator', 'logical_operator']
+
+    left_value = generate_program(max_depth - 1, terminal_nodes, function_nodes, options)
+    right_value = generate_program(max_depth - 1, terminal_nodes, function_nodes, options)
+    logical_operator = generate_logical_operator()
+
+    return Node(logical_operator.value, [left_value, right_value])
+
+
+def generate_if_statement():
+    condition = generate_condition(max_depth, terminal_nodes, function_nodes)
+    return Node('if', [condition, generate_block(max_depth, terminal_nodes, function_nodes)])
+
+
+def generate_program(max_depth, terminal_nodes, function_nodes, options=None):
+    if max_depth == 0:
+        return random.choice([generate_number,])()
+
+    def_options = [
+        'new_variable',
+        'operator',
+        'number',
+        'comparison_operator',
+        'logical_operator',
+        'output_statement',
+        # 'if_statement',
+        # 'loop'
+        ]
+
+    if options != None:
+        def_options = options
+
+    value = random.choice(def_options)
+
+    if value == 'operator':
+        node = generate_operator()
+        node.children = [
+            generate_number(),
+            generate_number()
+        ]
+        return node
+    elif value == 'generate_logical_value':
+        return generate_logical_value()
+    elif value == 'new_variable':
+        node = generate_new_variable()
+        node.children = [
+            generate_program(max_depth, terminal_nodes, function_nodes, ['assignment',])
+        ]
+        return node
+    elif value == 'number':
+        return generate_number()
+    elif value == 'comparison_operator':
+        node = generate_comparison_operator()
+        node.children = [
+            generate_program(max_depth, terminal_nodes, function_nodes, ['generate_number_or_used_values']),
+            generate_program(max_depth, terminal_nodes, function_nodes, ['generate_number_or_used_values'])
+        ]
+        return node
+    elif value == 'generate_number_or_used_values':
+        return generate_number_or_used_values()
+    elif value == 'logical_operator':
+        node = generate_logical_operator()
+        node.children = [
+            generate_program(max_depth, terminal_nodes, function_nodes, ['generate_logical_value', 'comparison_operator']),
+            generate_program(max_depth, terminal_nodes, function_nodes, ['generate_logical_value', 'comparison_operator'])
+        ]
+        return node
+    elif value == 'assignment':
+        node = generate_assignment()
+        node.children = [
+            generate_program(max_depth, terminal_nodes, function_nodes, ['generate_number_or_used_values', 'comparison_operator', 'operator', 'generate_logical_value'])
+        ]
+        return node
+    elif value == 'output_statement':
+        node = generate_output_statement()
+        node.children = [
+            generate_program(max_depth, terminal_nodes, function_nodes, ['generate_number_or_used_values', 'comparison_operator', 'operator', 'generate_logical_value'])
+        ]
+        return node
+    elif value == 'if_statement':
+        node = generate_if_statement()
+        node.children = [generate_comparison_operator(),
+                         generate_program(max_depth, terminal_nodes, function_nodes, ['generate_number_or_used_values']),
+                         generate_program(max_depth, terminal_nodes, function_nodes, ['generate_number_or_used_values'])
+                         ]
+        return node
+    elif value == 'loop':
+        node = generate_loop()
+        node.children = [generate_comparison_operator(),
+                         generate_block(max_depth, terminal_nodes, function_nodes)]
+        return node
+
+
+def generate_code(program):
+    if isinstance(program.value, str):
+        return program.value + " " + " ".join(generate_code(child) for child in program.children)
+    else:
+        return program.value
+
 
 def crossover(parent1, parent2):
     child1 = copy.deepcopy(parent1)
     child2 = copy.deepcopy(parent2)
 
-    # Ensure that both parents have non-empty children
     if not child1.children or not child2.children:
         return child1, child2
 
-    # Swap subtrees
     subtree1 = random.choice(child1.children)
+
+    if not child2.children:
+        return child1, child2
+
     subtree2 = random.choice(child2.children)
 
     subtree1_index = child1.children.index(subtree1)
@@ -37,119 +201,112 @@ def crossover(parent1, parent2):
 
     return child1, child2
 
+
 def mutate(program, max_depth, terminal_nodes, function_nodes):
     mutated_program = copy.deepcopy(program)
 
-    # Ensure that the program has non-empty children
-    if not mutated_program.children:
+    if len(mutated_program.children) == 0:
         return mutated_program
 
-    # Randomly choose a node to mutate
     node_to_mutate = random.choice(mutated_program.children)
 
-    # Mutate value or subtree
     if random.random() < 0.5:
-        node_to_mutate.value = random.uniform(-1.0, 1.0)
+        if node_to_mutate.value == 'variable':
+            node_to_mutate.value = generate_number().value
+        elif node_to_mutate.value == 'comparison_operator':
+            node_to_mutate.value = generate_logical_operator().value
+        elif node_to_mutate.value == 'logical_operator':
+            node_to_mutate.value = generate_comparison_operator().value
     else:
         subtree_index = mutated_program.children.index(node_to_mutate)
-        mutated_program.children[subtree_index] = generate_random_program(
-            max_depth - 1, terminal_nodes, function_nodes)
+        if mutated_program.children[subtree_index].value in ['=',]:
+            mutated_program.children[subtree_index].children[0] = generate_number()
+        else:
+            mutated_program.children[subtree_index] = generate_program(max_depth - 1, terminal_nodes, function_nodes)
 
     return mutated_program
 
 
+'''Function for tensting program'''
+
 def evaluate_program(program, input_data):
-    predictions = []
+    # Implement your specific evaluation function here
+    pass
 
-    for input_point in input_data:
-        prediction = evaluate_node(program, input_point)
-        predictions.append(prediction)
-
-    return predictions
-
-def evaluate_node(node, input_point):
-    """Add values from language"""
-    if isinstance(node.value, float):
-        return node.value  # Terminal node (constant)
-    else:
-        if node.value == '+':
-            return evaluate_node(node.children[0], input_point) + evaluate_node(node.children[1], input_point)
-        elif node.value == '-':
-            return evaluate_node(node.children[0], input_point) - evaluate_node(node.children[1], input_point)
-        elif node.value == '*':
-            return evaluate_node(node.children[0], input_point) * evaluate_node(node.children[1], input_point)
-        elif node.value == '/':
-            denominator = evaluate_node(node.children[1], input_point)
-            if denominator != 0:
-                return evaluate_node(node.children[0], input_point) / denominator
-            else:
-                return 0  # Avoid division by zero
 
 def tournament_selection(population, k):
-    tournament = random.sample(population, k)
-    # return min(tournament, key=lambda program: adaptation_function(output_data, evaluate_program(program, input_data)))
-    return tournament[-1]
+    selected = []
+    for _ in range(len(population)):
+        tournament = random.sample(population, k)
+        # winner = min(tournament, key=lambda program: evaluate_program(program, input_data))
+        winner = tournament[0]
+        selected.append(winner)
+
+    if len(selected) % 2 != 0:
+        selected.pop()
+
+    parent_tuples = [(selected[i], selected[i + 1]) for i in range(0, len(selected), 2)]
+
+    return parent_tuples
+
 
 def serialize_program(program, filename):
     with open(filename, 'wb') as file:
         pickle.dump(program, file)
 
+
 def deserialize_program(filename):
     with open(filename, 'rb') as file:
         return pickle.load(file)
 
-'''Function for tensting program'''
+
+# '''Function for tensting program'''
 def adaptation_function(output, pred) -> float:
     pass
 
 
 def run(input_data, output_data, population_size, max_depth, terminal_nodes, function_nodes):
-    # Generate random population
-    population = [generate_random_program(max_depth, terminal_nodes, function_nodes) for _ in range(population_size)]
+    population = [generate_program(max_depth, terminal_nodes, function_nodes)
+                  for _ in range(population_size)]
+
     # Example evolution loop
-    for generation in range(30):
-        # Evaluate fitness
-        predictions = [evaluate_program(program, input_data) for program in population]
-        fitness_scores = [adaptation_function(output_data, prediction) for prediction in predictions]
+    # for generation in range(10):
+    #     # Evaluate fitness (implement your specific evaluation function)
+    #     fitness_scores = [evaluate_program(program, input_data) for program in population]
+    #
+    #     # Select parents using tournament selection
+    #     parents = tournament_selection(population, 2)
+    #
+    #     # Crossover and mutate
+    #     offspring = []
+    #     for parent_tuple in parents:
+    #         child1, child2 = crossover(parent_tuple[0], parent_tuple[1])
+    #         offspring.extend([mutate(child1, max_depth, terminal_nodes, function_nodes),
+    #                           mutate(child2, max_depth, terminal_nodes, function_nodes)])
+    #
+    #     # Replace old population with offspring
+    #     population = offspring
 
-        parents = [tournament_selection(population, 2) for _ in range(population_size)]
-
-        offspring = []
-        for i in range(0, population_size, 2):
-            child1, child2 = crossover(parents[i], parents[i + 1])
-            offspring.extend([mutate(child1, max_depth, terminal_nodes, function_nodes),
-                             mutate(child2, max_depth, terminal_nodes, function_nodes)])
-
-        # Replace old population with offspring
-        population = offspring
     return population
+
 
 '''
 Displaying the output tree in a transparent way
 '''
-def printProgram(program):
-    output = list()
-    if len(output) > 0:
-        printChildren(program, output)
-    print(output)
 
-def printChildren(program, output):
-    if len(program.children) > 1:
-        output.append('(')
-        if len(program.children[0].children) > 0:
-            printChildren(program.children[0])
-        else:
-            output.append(program.children[0].value)
 
-        output.append(program.value)
-
-        if len(program.children[1].children) > 0:
-            printChildren(program.children[1])
-        else:
-            output.append(program.children[1].value)
-        output.append(')')
+def display_all_nodes(program, indent=0):
+    if program == None:
+        return
+    if program.value in {'&&', '||', '<', '<=', '==', '!=', '>', '>='}:
+        print("  " * indent + f"Operator: {program.value}")
+    elif program.value in {'true', 'false', '=', '+', '-', '*', '/'}:
+        print("  " * indent + f"Operator: {program.value}")
     else:
-        output.append(program.value)
+        print("  " * indent + f"Value: {program.value}")
+
+    for child in program.children:
+        display_all_nodes(child, indent + 1)
 
 
 max_depth = 5
@@ -160,7 +317,6 @@ output_data = 2 * input_data + np.sin(5 * input_data) + np.random.normal(0, 0.1,
 
 population_size = 10
 
-
 population = run(input_data, output_data, population_size, max_depth, terminal_nodes, function_nodes)
 # best_program = min(population, key=lambda program: adaptation_function(output_data, evaluate_program(program, input_data)))
 # best_program = population[0]
@@ -169,4 +325,5 @@ population = run(input_data, output_data, population_size, max_depth, terminal_n
 # deserialized_program = deserialize_program('serialized_program_regression.pkl')
 
 for i in population:
-    printProgram(i)
+    display_all_nodes(i)
+    print('------------------------------------')
