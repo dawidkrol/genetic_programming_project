5ac280bc2489c4528af8777280b02bd392fdab13
Dawid Król <dawidkrol7@gmail.com>
2024-01-09 17:58:19 +0100

    Implementing sepearte variables set for each program and graph creating for avg and max fitness

--- GP/GP_lib.py
+++ GP/GP_lib.py
@@ -2,8 +2,14 @@ import random
 import numpy as np
 import program_variables as PV
 import program_evaluator as evaluator
+import matplotlib.pyplot as plt
 
 used_variables = set()
+
+class Program:
+    def __init__(self):
+        self.variables = set()
+        self.program = []
 
 
 class Node:
@@ -12,11 +18,11 @@ class Node:
         self.children = children or []
 
 
-def generate_new_variable():
+def generate_new_variable(variables: set):
     new_variable = 'i' + str(random.randint(1, 99999))
-    while new_variable in used_variables:
+    while new_variable in variables:
         new_variable = 'i' + str(random.randint(1, 99999))
-    used_variables.add(new_variable)
+    variables.add(new_variable)
     return new_variable
 
 
@@ -34,7 +40,7 @@ def generate_number(options=None):
         return Node(str(random.randint(0, 9)))
 
 
-def generate_number_or_used_values():
+def generate_number_or_used_values(variables):
     options = ['int', 'input']
     if len(used_variables) > 0:
         options.append('used_variables')
@@ -45,7 +51,7 @@ def generate_number_or_used_values():
     elif option == 'input':
         return Node('input')
     elif option == 'used_variables':
-        return Node(str(random.choice(list(used_variables))))
+        return Node(str(random.choice(list(variables))))
 
 
 def generate_comparison_operator():
@@ -68,14 +74,14 @@ def generate_loop():
     return Node('while ')
 
 
-def generate_block(max_depth):
+def generate_block(max_depth, variables):
     if max_depth <= 2:
-        return generate_program(max_depth - 1, [
+        return generate_program(max_depth - 1, variables, [
             'new_variable',
             'output_statement',
         ])
     else:
-        return generate_program(max_depth - 1, [
+        return generate_program(max_depth - 1, variables, [
             'new_variable',
             'output_statement',
             'if_statement',
@@ -87,11 +93,11 @@ def generate_logical_value():
     return Node(random.choice(['true', 'false']))
 
 
-def generate_condition(max_depth):
+def generate_condition(max_depth, variables):
     options = ['number', 'constant', 'comparison_operator', 'logical_operator']
 
-    left_value = generate_program(max_depth - 1, options)
-    right_value = generate_program(max_depth - 1, options)
+    left_value = generate_program(max_depth - 1, variables, options)
+    right_value = generate_program(max_depth - 1, variables, options)
     logical_operator = generate_logical_operator()
 
     return Node(logical_operator.value, [left_value, right_value])
@@ -101,7 +107,7 @@ def generate_if_statement():
     return Node('if')
 
 
-def generate_program(max_depth, options=None, isMutation=False):
+def generate_program(max_depth, variables: set, options=None, isMutation=False):
     if max_depth <= 3:
         def_options = [
             'new_variable',
@@ -139,9 +145,9 @@ def generate_program(max_depth, options=None, isMutation=False):
     elif value == 'generate_logical_value':
         return generate_logical_value()
     elif value == 'new_variable':
-        node = Node(generate_new_variable())
+        node = Node(generate_new_variable(variables))
         node.children = [
-            generate_program(max_depth - 1, ['assignment', ])
+            generate_program(max_depth - 1, variables, ['assignment', ])
         ]
         return node
     elif value == 'number':
@@ -149,44 +155,44 @@ def generate_program(max_depth, options=None, isMutation=False):
     elif value == 'comparison_operator':
         node = generate_comparison_operator()
         node.children = [
-            generate_program(max_depth - 1, ['generate_number_or_used_values', 'comparison_operator']),
-            generate_program(max_depth - 1, ['generate_number_or_used_values', 'comparison_operator'])
+            generate_program(max_depth - 1, variables, ['generate_number_or_used_values', 'comparison_operator']),
+            generate_program(max_depth - 1, variables, ['generate_number_or_used_values', 'comparison_operator'])
         ]
         return node
     elif value == 'generate_number_or_used_values':
-        return generate_number_or_used_values()
+        return generate_number_or_used_values(variables)
     elif value == 'logical_operator':
         node = generate_logical_operator()
         node.children = [
-            generate_program(max_depth - 1, ['generate_logical_value', 'comparison_operator']),
-            generate_program(max_depth - 1, ['generate_logical_value', 'comparison_operator'])
+            generate_program(max_depth - 1, variables, ['generate_logical_value', 'comparison_operator']),
+            generate_program(max_depth - 1, variables, ['generate_logical_value', 'comparison_operator'])
         ]
         return node
     elif value == 'assignment':
         node = generate_assignment()
         node.children = [
-            generate_program(max_depth - 1, ['generate_number_or_used_values', 'comparison_operator', 'operator',
+            generate_program(max_depth - 1, variables, ['generate_number_or_used_values', 'comparison_operator', 'operator',
                                              'generate_logical_value'])
         ]
         return node
     elif value == 'output_statement':
         node = generate_output_statement()
         node.children = [
-            generate_program(max_depth - 1, ['generate_number_or_used_values', 'comparison_operator', 'operator',
+            generate_program(max_depth - 1, variables, ['generate_number_or_used_values', 'comparison_operator', 'operator',
                                              'generate_logical_value'])
         ]
         return node
     if value in {'if_statement', 'loop'}:
         node = generate_if_statement() if value == 'if_statement' else generate_loop()
-        condition = generate_program(3, ['comparison_operator'])
+        condition = generate_program(3, variables, ['comparison_operator'])
 
         if condition is None:
-            condition = generate_program(3, ['comparison_operator'])
+            condition = generate_program(3, variables, ['comparison_operator'])
 
-        body = generate_block(max_depth - 1)
+        body = generate_block(max_depth - 1, variables)
 
         if body is None:
-            body = generate_block(max_depth - 1)
+            body = generate_block(max_depth - 1, variables)
 
         node.children = [condition, body]
         return node
@@ -252,7 +258,7 @@ def return_part(program):
         return f'({result})' if program.children else result
 
 
-def generate_random_statement():
+def generate_random_statement(variables):
     options = [
         'new_variable',
         'output_statement',
@@ -261,7 +267,7 @@ def generate_random_statement():
     ]
     choice = random.choice(options)
     if choice == 'new_variable':
-        return generate_new_variable()
+        return generate_new_variable(variables)
     elif choice == 'output_statement':
         return generate_output_statement()
     elif choice == 'if_statement':
@@ -270,30 +276,30 @@ def generate_random_statement():
         return generate_loop()
 
 
-def mutate_program(program, max_depth, max_width, mutation_rate):
+def mutate_program(program, max_depth, max_width, mutation_rate, variables):
     if isinstance(program, list):
         for i in range(1, max_width - 1):
             j = random.randint(0, len(program) - 1)
-            program[j] = mutate_program(program[j], max_depth - 1, max_width, mutation_rate)
+            program[j] = mutate_program(program[j], max_depth - 1, max_width, mutation_rate, variables)
         return program
 
     if random.randint(0, 100)/100 < mutation_rate:
-        return generate_program(random.randint(1, max_depth + 2), None, True)
+        return generate_program(random.randint(1, max_depth + 2), variables, None, True)
     elif program is not None:
         for child in program.children:
             if max_depth >= 0:
-                mutate_program(child, max_depth - 1, max_width, mutation_rate)
+                mutate_program(child, max_depth - 1, max_width, mutation_rate, variables)
     return program
 
 
-def crossover(parent1, parent2):
-    parent1 = [node for node in parent1]
-    parent2 = [node for node in parent2]
+def crossover(parent1: Program, parent2: Program):
+    parent1_nodes = [node for node in parent1.program]
+    parent2_nodes = [node for node in parent2.program]
 
-    cross_point1 = random.randint(0, len(parent1) - 1)
-    cross_point2 = random.randint(0, len(parent2) - 1)
+    cross_point1 = random.randint(0, len(parent1_nodes) - 1)
+    cross_point2 = random.randint(0, len(parent2_nodes) - 1)
 
-    parent1[cross_point1], parent2[cross_point2] = parent2[cross_point2], parent1[cross_point1]
+    parent1.program[cross_point1], parent2.program[cross_point2] = parent2_nodes[cross_point2], parent1_nodes[cross_point1]
 
     return parent1, parent2
 
@@ -332,7 +338,9 @@ def fitness(program, input_data, target_output):
 
 def return_program(program):
     output = ""
-    if isinstance(program, list):
+    if isinstance(program, Program):
+        output += return_program(program.program)
+    elif isinstance(program, list):
         for i, part in enumerate(program):
             output += return_part(part)
             output += "\n"
@@ -344,7 +352,9 @@ def return_program(program):
 
 def generate_program_base(max_depth, max_width):
     width = random.randint(1, max_width)
-    return [generate_program(max_depth) for _ in range(width)]
+    pp = Program()
+    pp.program = [generate_program(max_depth, pp.variables) for _ in range(width)]
+    return pp
 
 
 def add_to_programs(program, fitness, generation):
@@ -355,21 +365,6 @@ def add_to_programs(program, fitness, generation):
         file.write(f"Fitness: {fitness}\n\n")
         file.write(serialized_program)
 
-def tournament_selection(population, fitness_scores, tournament_size):
-    new_population = []
-    population_size = len(population)
-
-    while len(new_population) < population_size:
-        tournament = random.sample(population, tournament_size)
-        tournament_witch_fitness = []
-        for i in range(tournament_size):
-            tournament_witch_fitness.append((tournament[i], fitness_scores[population.index(tournament[i])]))
-
-        winner = max(tournament_witch_fitness, key=lambda x: x[1])
-        new_population.append(winner)
-
-    return new_population
-
 def process_data(data):
     output = []
     for line in data:
@@ -377,6 +372,8 @@ def process_data(data):
     return [[int(x) for x in z] for z in output]
 
 def run(input_data, output_data, population_size, max_depth, max_width, generations):
+    avg_fitnesses = []
+    max_fitnesses = []
     output_data = process_data(output_data)
     input_data = process_data(input_data)
     serialize_program("", './program.txt')
@@ -384,7 +381,6 @@ def run(input_data, output_data, population_size, max_depth, max_width, generati
         file.write("")
     population = []
     for _ in range(population_size):
-        used_variables.clear()
         population.append(generate_program_base(max_depth, max_width))
 
     max_fitness = -1
@@ -411,8 +407,15 @@ def run(input_data, output_data, population_size, max_depth, max_width, generati
         print(f'______________________{generation}_____________________________')
         print(f'Max Fitness: {fitness_scores[max_fitness_index]}')
         print(f'Avg Fitness: {avg_fitness}')
+        avg_fitnesses.append(avg_fitness)
+        max_fitnesses.append(fitness_scores[max_fitness_index])
+        plt.axis([0, generation + 1, -1.0, 1.0])
+        plt.plot(range(0, generation + 1), avg_fitnesses, label="Avg Fitness")
+        plt.plot(range(0, generation + 1), max_fitnesses, label="Max Fitness")
+        plt.legend()
+        plt.pause(0.05)
 
-        if fitness_scores[max_fitness_index] >= 0.95:
+        if fitness_scores[max_fitness_index] >= 1.0:
             print("__________!!! SOLVED !!!__________")
             print(f"______________GENERATION: {generation}_________________")
             serialized_program = return_program(best_program)
@@ -423,9 +426,9 @@ def run(input_data, output_data, population_size, max_depth, max_width, generati
             return population
 
         max_fitness_program = population[max_fitness_index]
-        mutated_max_fitness_program = mutate_program(max_fitness_program, PV.MAX_DEPTH - 1, PV.MAX_WIDTH,
-                                                     PV.MUTATION_RATE)
-        population[max_fitness_index] = mutated_max_fitness_program
+        max_fitness_program.program = mutate_program(max_fitness_program.program, PV.MAX_DEPTH - 1, PV.MAX_WIDTH,
+                                                     PV.MUTATION_RATE, max_fitness_program.variables)
+        population[max_fitness_index] = max_fitness_program
 
         parents = random.sample(population, 2)
         children = crossover(*parents)
